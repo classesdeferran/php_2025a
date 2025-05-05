b4527e02bf1914b50df001469e997ad8871b9cdf
= <=>
2025-05-05 12:44:00 +0200

    2025-05-05

--- 07_BD_PDO/insert.php
+++ 07_BD_PDO/insert.php
@@ -1,8 +1,18 @@
 <?php
+// Llamar al fichero de la conexión
+require_once 'conexion.php';
+
 
 // print_r($_POST);
-echo $_POST['usuario'];
-echo "<br>";
-echo $_POST['color'];
+// echo "<br>";
+// echo $_POST['usuario'];
+// echo $_POST['color'];
 
-$insert = "";
+$insert = "INSERT INTO colores(color, usuario) VALUES (?, ?);";
+$insert_prepare = $conn->prepare($insert);
+$insert_prepare->execute([$_POST['color'], $_POST['usuario']]);
+
+$insert_prepare = null;
+$conn = null;
+
+header('location:index.php');
